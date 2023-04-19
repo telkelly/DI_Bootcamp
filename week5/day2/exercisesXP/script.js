5a1169472b7946ddfa605b0107ff5e949954ca78
Kelly <84016657+telkelly@users.noreply.github.com>
2023-04-19 20:58:45 +0300

    exercises using dom event

---
+++ week5/day2/exercisesXP/script.js
@@ -0,0 +1,38 @@
+// exercise-1
+
+const h1 = document.getElementsByTagName('h1');
+const article = document.body.firstElementChild;
+const lastParagr = article.lastElementChild;
+const h2 = article.children[1];
+const h3 = article.children[2];
+const allParagr = document.getElementsByTagName('p');
+console.log(allParagr[1]);
+console.log(h1[0]);
+
+h2.addEventListener('click', function (e) {
+    e.target.style.background = 'red';
+})
+
+h3.addEventListener("click", function (e) {
+  e.target.style.display = "none";
+});
+
+article.removeChild(lastParagr);
+
+function getBold() {
+    for (let i = 0; i < allParagr.length; i++){
+        allParagr[i].style.fontWeight = '900'
+    }
+}
+
+h1[0].addEventListener('mouseover', function (e) {
+    let randomNum = Math.floor(Math.random() * 100);
+    e.target.style.fontSize = `${randomNum}px`;
+})
+
+allParagr[1].addEventListener('mouseover', function (e) {
+    e.target.classList.add('fade-in');
+})
+
+
+
