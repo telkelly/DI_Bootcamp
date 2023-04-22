d112eb0e4e6ec0aad3d310f55e7f493db3c71c4d
Kelly <84016657+telkelly@users.noreply.github.com>
2023-04-22 22:19:05 +0300

    completed exercise using forms and dom events

--- week5/day2/exercisesXP/exercise-2/script.js
+++ week5/day2/exercisesXP/exercise-2/script.js
@@ -1,19 +1,24 @@
 const form = document.body.firstElementChild;
-const fname = document.getElementById('fname');
+const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
-const ul = document.getElementsByClassName('usersAnswer')
-const atttributeName = document.querySelector('[name=fname]').value;
+const ul = document.getElementsByClassName("usersAnswer");
+const atttributeName = document.querySelector("[name=fname]").value;
 const atttributeLname = document.querySelector("[name=lname]").value;
-const submitBtn = document.getElementById('submit');
+const submitBtn = document.getElementById("submit");
 
-const valuefName = fname.value;
-const valuelName = lname.value;
-
-submitBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-        let newLi = document.createElement('li');
-        let newText = document.createTextNode(valuefName)
-        newLi.appendChild(newText)
-        ul[0].appendChild(newLi);
-    
-})
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const valuelName = lname.value;
+  const valuefName = fname.value;
+  if (valuefName == "" || valuelName == "") {
+    alert("Please write your name and last name");
+  }
+  let newLiName = document.createElement("li");
+  let newLiLastname = document.createElement("li");
+  let newTextName = document.createTextNode(valuefName);
+  let newTextLastname = document.createTextNode(valuelName);
+  newLiName.appendChild(newTextName);
+  newLiLastname.appendChild(newTextLastname);
+  ul[0].appendChild(newLiName);
+  ul[0].appendChild(newLiLastname);
+});
