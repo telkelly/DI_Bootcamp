90f543c76111938874d837d62ee0ff92793334a8
Kelly <84016657+telkelly@users.noreply.github.com>
2023-04-28 23:07:02 +0300

    changed style for main div and title div, also created gameOver function

--- HackatonProject/script.js
+++ HackatonProject/script.js
@@ -3,9 +3,18 @@ const different = document.getElementsByClassName("different").item(0);
 const rulesBtn = document.getElementById("rules-btn");
 const rulesDiv = document.getElementsByClassName("rules").item(0);
 const mainDiv = document.getElementsByClassName("main-div").item(0);
+const counter = document.getElementById("counter");
+
+console.log(counter);
 
 let scope = 0;
 let isCalledOnce = true;
+let isSquare = false;
+
+function scopeCounter() {
+  scope++;
+  counter.textContent = scope;
+}
 
 rulesBtn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -14,8 +23,8 @@ rulesBtn.addEventListener("click", function (e) {
     const newText = document.createTextNode(
       "The game is about to find different square color within these squares. You'll gain one point when find it. Every level is getting harder so try to gain as much point as you can."
     );
-      newParag.append(newText);
-      newParag.classList.add('rules-info')
+    newParag.append(newText);
+    newParag.classList.add("rules-info");
     rulesDiv.appendChild(newParag);
     isCalledOnce = false;
   } else {
@@ -26,8 +35,27 @@ rulesBtn.addEventListener("click", function (e) {
 
 different.addEventListener("click", function (e) {
   e.preventDefault();
+  scopeCounter();
   secondLevel();
 });
+
+function gameOver() {
+  for (let square of squares) {
+    if (square) {
+      square.addEventListener("click", function (e) {
+        return alert("bye");
+      });
+    } else {
+      return false;
+    }
+  }
+}
+
+// if (isDifferent) {
+//     scopeCounter();
+// } else {
+//     alert('Game over')
+// }
 
 function secondLevel() {
   mainDiv.innerHTML = "";
@@ -39,13 +67,18 @@ function secondLevel() {
   }
   getRandom();
   const different = document.getElementsByClassName("different").item(0);
+  different.classList.remove("square");
   different.style.background = "rgb(189, 121, 66)";
+  gameOver();
   different.addEventListener("click", function (e) {
     thirdLevel();
   });
 }
 
+console.log(scope);
+
 function thirdLevel() {
+  scopeCounter();
   mainDiv.innerHTML = "";
   for (let i = 0; i < 9; i++) {
     let newDiv = document.createElement("div");
@@ -55,6 +88,7 @@ function thirdLevel() {
   }
   getRandom();
   const different = document.getElementsByClassName("different").item(0);
+  different.classList.remove("square");
   different.style.background = "purple";
   different.addEventListener("click", function (e) {
     forthLevel();
@@ -62,6 +96,7 @@ function thirdLevel() {
 }
 
 function forthLevel() {
+  scopeCounter();
   mainDiv.innerHTML = "";
   for (let i = 0; i < 12; i++) {
     let newDiv = document.createElement("div");
@@ -71,27 +106,32 @@ function forthLevel() {
   }
   getRandom();
   const different = document.getElementsByClassName("different").item(0);
+  different.classList.remove("square");
   different.style.background = "darkgreen";
   different.addEventListener("click", function (e) {
-    FifthLevel();
+    fifthLevel();
   });
 }
 
-function FifthLevel() {
+function fifthLevel() {
+  scopeCounter();
   mainDiv.innerHTML = "";
-  for (let i = 0; i < 12; i++) {
+  for (let i = 0; i < 30; i++) {
     let newDiv = document.createElement("div");
-      newDiv.classList.add("square");
-      newDiv.style.borderRadius = '50%';
-      newDiv.style.border = '5px solid yellow'
-          newDiv.style.background = "green";
+    newDiv.classList.add("square");
+    newDiv.style.borderRadius = "50%";
+    newDiv.style.border = "5px solid yellow";
+    newDiv.style.width = "40px";
+    newDiv.style.height = "40px";
+    newDiv.style.background = "green";
     mainDiv.appendChild(newDiv);
   }
   getRandom();
   const different = document.getElementsByClassName("different").item(0);
+  different.classList.remove("square");
   different.style.background = "darkgreen";
   different.addEventListener("click", function (e) {
-    FifthLevel();
+    fifthLevel();
   });
 }
 
