7aa3146f6d29ebd4394281f15437f0b3173769a0
Kelly <84016657+telkelly@users.noreply.github.com>
2023-05-04 19:04:15 +0300

    started generate a random quote app

---
+++ week6/day5/mini-project-1/script.js
@@ -0,0 +1,67 @@
+const quote = document.getElementById("quote");
+
+let quotes = [
+  {
+    id: 0,
+    author: "Oscar Wilde",
+    quote: "Be yourself; everyone else is already taken.",
+  },
+  {
+    id: 1,
+    author: "Albert Einstein",
+    quote:
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+  },
+  {
+    id: 2,
+    author: "Bernard M. Baruch",
+    quote:
+      "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+  },
+  {
+    id: 3,
+    author: "Oscar Wilde",
+    quote: "Always forgive your enemies; nothing annoys them so much.",
+  },
+  {
+    id: 4,
+    author: "Friedrich Nietzsche",
+    quote: "Without music, life would be a mistake.",
+  },
+  {
+    id: 5,
+    author: "H. Jackson Brown Jr.",
+    quote:
+      "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.",
+  },
+  {
+    id: 6,
+    author: "Will Ferrell",
+    quote:
+      "Before you marry a person, you should first make them use a computer with slow Internet to see who they really are.",
+  },
+  {
+    id: 7,
+    author: "Ellen DeGeneres",
+    quote:
+      "Never follow anyone else’s path. Unless you’re in the woods and you’re lost and you see a path. Then by all means follow that path.",
+  },
+];
+
+const generateRandomQuote = () => {
+//   Object.keys(quotes).forEach((key) => {
+//     let newQuote, newAuthor, id;
+//     let newParag = document.createElement("p");
+//     let newSpan = document.createElement("span");
+
+//     newId = quotes[key].id;
+//     newQuote = quotes[key].quote;
+//     newAuthor = quotes[key].author;
+
+//     newParag.append(newQuote);
+//     newSpan.append(newAuthor);
+
+//     console.log(quotes[key].author);
+//   });
+
+};
