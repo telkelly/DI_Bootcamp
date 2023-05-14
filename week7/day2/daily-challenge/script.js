214d806b9f14258d08577070751fd60a25d79f88
Kelly <84016657+telkelly@users.noreply.github.com>
2023-05-14 22:18:57 +0300

    completed exrcise using advanced array methods

--- week7/day2/daily-challenge/script.js
+++ week7/day2/daily-challenge/script.js
@@ -1,3 +1,5 @@
+// part 1
+
 const inventory = [
   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
@@ -12,12 +14,22 @@ const inventory = [
 ];
 
 function getCarHonda(carInventory) {
-    let founded = carInventory.find(i => {
-        return i.car_make === "Honda";
-    })
-    return console.log(
-      `This is a ${founded.car_make} ${founded.car_model} from ${founded.car_year}`
-    );
+  let founded = carInventory.find((i) => {
+    return i.car_make === "Honda";
+  });
+  return console.log(
+    `This is a ${founded.car_make} ${founded.car_model} from ${founded.car_year}`
+  );
 }
 
-getCarHonda(inventory)
+getCarHonda(inventory);
+
+// part 2
+
+function sortCarInventoryByYear(carInventory) {
+  return carInventory.sort((a, b) => a.car_year - b.car_year);
+}
+
+const sortedInventory = sortCarInventoryByYear(inventory);
+
+console.log(sortedInventory);
