70f575f8d48f329b7095d5f4514b5fec0c4aa71a
Kelly <84016657+telkelly@users.noreply.github.com>
2023-05-14 06:07:56 +0300

    started exercises

---
+++ week7/day4/exercisesXP/script.js
@@ -0,0 +1,26 @@
+// exercise-1
+
+const person = {
+  name: "John Doe",
+  age: 25,
+  location: {
+    country: "Canada",
+    city: "Vancouver",
+    coordinates: [49.2827, -123.1207],
+  },
+};
+
+const {
+  name,
+  location: {
+    country,
+    city,
+    coordinates: [lat, lng],
+  },
+} = person;
+
+console.log(
+  `I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`
+); // it'll copy the object
+
+// exercise-2
