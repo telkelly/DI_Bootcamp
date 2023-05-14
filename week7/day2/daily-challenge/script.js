b57396615743c062e5b9e27ab86fe36e8500d252
Kelly <84016657+telkelly@users.noreply.github.com>
2023-05-14 22:05:47 +0300

    doing daile challenge

---
+++ week7/day2/daily-challenge/script.js
@@ -0,0 +1,23 @@
+const inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  {
+    id: 4,
+    car_make: "Land Rover",
+    car_model: "Defender Ice Edition",
+    car_year: 2010,
+  },
+  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+
+function getCarHonda(carInventory) {
+    let founded = carInventory.find(i => {
+        return i.car_make === "Honda";
+    })
+    return console.log(
+      `This is a ${founded.car_make} ${founded.car_model} from ${founded.car_year}`
+    );
+}
+
+getCarHonda(inventory)
