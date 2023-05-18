101d7e9fdcd839a1d4e3177ff0ce6fc0ecddc2c5
Kelly <84016657+telkelly@users.noreply.github.com>
2023-05-18 22:30:19 +0300

    made simple html form that printed full name as JSON on HTML

--- week8/day2/daily-challenge/script.js
+++ week8/day2/daily-challenge/script.js
@@ -1,5 +1,10 @@
 function getFullName(e) {
     e.preventDefault();
     const frm = document.forms.nameForm;
-    console.log(frm);
+    let name = frm.elements.name.value;
+    let lastname = frm.elements.lastName.value;
+    let obj = { name, lastname };
+    let newJSON = JSON.stringify(obj);
+    let div = document.getElementsByClassName('new').item(0);
+    return div.innerHTML = newJSON;
 }
