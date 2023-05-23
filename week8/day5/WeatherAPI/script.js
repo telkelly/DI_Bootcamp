2cc7dbf67b4f0833dcfc569574650529c79ebed2
Kelly <84016657+telkelly@users.noreply.github.com>
2023-05-23 15:08:00 +0300

    got data for weather div and added reset css to style

--- week8/day5/WeatherAPI/script.js
+++ week8/day5/WeatherAPI/script.js
@@ -3,15 +3,17 @@ const APIkey = "6bc236fa8bd5e7e03f83fd8cea3eac74";
 let lat = 0;
 let lon = 0;
 
-function getCoordinates(country) {
+function getCoordinates(e) {
+  e.preventDefault();
+  let formCountry = document.forms.formCountry;
+  let country = formCountry.elements.inputCountry.value;
+
   let url =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     country +
     "&limit=5&appid=" +
     APIkey;
   let xhr = new XMLHttpRequest();
-
-  // xhr.responseType = "json";
 
   xhr.open("GET", url);
   xhr.send();
@@ -20,22 +22,43 @@ function getCoordinates(country) {
     let json = JSON.parse(xhr.responseText);
     lat = json[0].lat;
     lon = json[0].lon;
-    console.log(lat, lon);
-    return lat, lon;
+    getData(lat, lon);
   };
 }
 
-function getData(e) {
-  e.preventDefault();
-  console.log(lat,lon);
-
+function getData(lat, lon) {
   let url =
-    "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=" +
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=` +
     APIkey;
+
   let xhr = new XMLHttpRequest();
 
   xhr.open("GET", url);
+  xhr.send();
+  xhr.responseType = "json";
+  xhr.onload = function () {
+    const dataMain = xhr.response.main;
+    const weather = document.getElementsByClassName("weather").item(0);
+    let formulaToCelsium = Math.floor(dataMain.temp - 273.15);
+
+    let name = document.createElement('h4');
+    let newH3 = document.createElement("h3");
+    let newImg = document.createElement("img");
+    let newText = document.createElement('p');
+
+    name.append(`${xhr.response.name}, ${xhr.response.sys.country}`);
+    newImg.setAttribute("src", getWeatherIcon(xhr.response.weather[0].icon));
+    newH3.append(formulaToCelsium);
+    newText.append(xhr.response.weather[0].description);
+    console.log(xhr.response.name);
+
+    weather.appendChild(name)
+    weather.appendChild(newImg);
+    weather.appendChild(newH3);
+    weather.appendChild(newText)
+  };
 }
 
-getCoordinates("Israel");
-console.log(lat, lon);
+function getWeatherIcon(iconCode) {
+  return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+}
