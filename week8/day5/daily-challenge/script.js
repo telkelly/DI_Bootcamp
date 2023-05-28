5c12b6bb9a2d9378aca7758c8716c820a0694a8a
Kelly <84016657+telkelly@users.noreply.github.com>
2023-05-28 19:12:06 +0300

    solved exercise using padStart()

---
+++ week8/day5/daily-challenge/script.js
@@ -0,0 +1,14 @@
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
+  const second = String(date.getSeconds()).padStart(2, "0");
+
+  return `${year}${month}${day}${hour}${minute}${second}`;
+}
+
+const date = new Date();
+const formattedDate = formatDate(date);
+console.log(formattedDate);
