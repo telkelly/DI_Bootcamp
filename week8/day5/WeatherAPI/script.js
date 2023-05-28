0c2d09f6453a59b66a5347c9196b31339de22b4a
Kelly <84016657+telkelly@users.noreply.github.com>
2023-05-28 19:05:51 +0300

    added checkbox that change temp to Fahrenheit and Celsius also added div for error input. Need good css for these elements

--- week8/day5/WeatherAPI/script.js
+++ week8/day5/WeatherAPI/script.js
@@ -1,5 +1,7 @@
 const APIkey = "6bc236fa8bd5e7e03f83fd8cea3eac74";
 
+const weathers = document.getElementsByClassName("weathers").item(0);
+
 let lat = 0;
 let lon = 0;
 let flag = false;
@@ -21,73 +23,106 @@ function getCoordinates(e) {
 
   xhr.onload = function () {
     let json = JSON.parse(xhr.responseText);
-    lat = json[0].lat;
-    lon = json[0].lon;
-    getData(lat, lon);
+    try {
+      lat = json[0].lat;
+      lon = json[0].lon;
+      getData(lat, lon);
+    } catch {
+      weathers.innerHTML = "";
+      let newDiv = document.createElement("div");
+      newDiv.classList.add("error");
+
+      let newText = document.createElement("h3");
+      let newText1 = document.createElement("h3");
+
+      newText.innerText = "Oops";
+      newDiv.appendChild(newText);
+
+      newText1.innerText = "There is no city, try again!";
+      newDiv.appendChild(newText1);
+
+      let newSpan = document.createElement("span");
+      newSpan.classList.add("close-err");
+      newDiv.appendChild(newSpan);
+
+      weathers.appendChild(newDiv);
+      
+    }
   };
 }
 
 function getData(lat, lon) {
-  let url =
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=` +
-    APIkey;
+  try {
+    let url =
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=` +
+      APIkey;
 
-  let xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
 
-  xhr.open("GET", url);
-  xhr.send();
-  xhr.responseType = "json";
-  xhr.onload = function () {
-    const switcher = document.getElementsByClassName("toggle").item(0);
+    xhr.open("GET", url);
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = function () {
+      const switcher = document.getElementsByClassName("switch").item(0);
 
-    const dataMain = xhr.response.main;
-    const weathers = document.getElementsByClassName("weathers").item(0);
-    let formulaToCelsium = `${Math.floor(dataMain.temp - 273.15)}°C`;
-    let formulaToFarh = `${Math.floor(
-      ((dataMain.temp - 273.15) * 9) / 5 + 32
-    )}°F`;
+      const dataMain = xhr.response.main;
+      let formulaToCelsium = `${Math.floor(dataMain.temp - 273.15)}°C`;
+      let formulaToFarh = `${Math.floor(
+        ((dataMain.temp - 273.15) * 9) / 5 + 32
+      )}°F`;
 
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("weather");
-    let name = document.createElement("h4");
-    name.classList.add("country-name");
-    let newH3 = document.createElement("h3");
-    newH3.classList.add("temp");
-    let newImg = document.createElement("img");
-    let newText = document.createElement("p");
-    newText.classList.add("weather-description");
-    let newSpan = document.createElement("span");
-    newSpan.classList.add("close");
+      let newDiv = document.createElement("div");
+      newDiv.classList.add("weather");
+      let name = document.createElement("h4");
+      name.classList.add("country-name");
+      let newH3 = document.createElement("h3");
+      newH3.classList.add("temp");
+      let newImg = document.createElement("img");
+      let newText = document.createElement("p");
+      newText.classList.add("weather-description");
+      let newSpan = document.createElement("span");
+      newSpan.classList.add("close");
 
-    name.append(`${xhr.response.name}, ${xhr.response.sys.country}`);
-    newImg.setAttribute("src", getWeatherIcon(xhr.response.weather[0].icon));
-    newH3.append(formulaToCelsium);
-    newText.append(xhr.response.weather[0].description);
-    console.log(xhr.response.name);
+      name.append(`${xhr.response.name}, ${xhr.response.sys.country}`);
+      newImg.setAttribute("src", getWeatherIcon(xhr.response.weather[0].icon));
+      newH3.append(formulaToCelsium);
+      newText.append(xhr.response.weather[0].description);
+      console.log(xhr.response.name);
 
-    newDiv.appendChild(name);
-    newDiv.appendChild(newImg);
-    newDiv.appendChild(newH3);
-    newDiv.appendChild(newText);
-    newDiv.appendChild(newSpan);
+      newDiv.appendChild(name);
+      newDiv.appendChild(newImg);
+      newDiv.appendChild(newH3);
+      newDiv.appendChild(newText);
+      newDiv.appendChild(newSpan);
 
-    weathers.appendChild(newDiv);
+      weathers.appendChild(newDiv);
 
-    newSpan.onclick = function () {
-      weathers.removeChild(newDiv);
+      newSpan.onclick = function () {
+        weathers.removeChild(newDiv);
+      };
+
+      switcher.onclick = function (e) {
+        e.stopPropagation();
+        if (flag === false) {
+          newH3.innerHTML = "";
+          newH3.append(formulaToFarh);
+          flag = true;
+          console.log(5);
+        } else {
+          newH3.innerHTML = "";
+          newH3.append(formulaToCelsium);
+          console.log(6);
+          flag = false;
+        }
+      };
     };
-
-    switcher.onclick = function (e) {
-      if (switcher.checked) {
-        console.log(5);
-      } else {
-        console.log(6);
-      }
+  } catch (error) {
+    if (error) {
+      console.log(10);
     }
   }
 }
 
-
-  function getWeatherIcon(iconCode) {
-    return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-  }
+function getWeatherIcon(iconCode) {
+  return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+}
