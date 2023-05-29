6c91d7b878c8d15afabfc8576179972405352688
Kelly <84016657+telkelly@users.noreply.github.com>
2023-05-29 16:53:56 +0300

    solved exercise using promise.all()

--- week9/day4/daily-exercises/script.js
+++ week9/day4/daily-exercises/script.js
@@ -14,3 +14,39 @@
 
 // exercise-2
 
+const form = document.getElementById("cityForm");
+const resultDiv = document.getElementById("result");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const city1Lat = document.getElementById("city1Lat").value;
+  const city1Long = document.getElementById("city1Long").value;
+  const city2Lat = document.getElementById("city2Lat").value;
+  const city2Long = document.getElementById("city2Long").value;
+
+  const city1Promise = fetchSunriseTime(city1Lat, city1Long);
+  const city2Promise = fetchSunriseTime(city2Lat, city2Long);
+
+  Promise.all([city1Promise, city2Promise])
+    .then(function (results) {
+      const city1SunriseTime = results[0];
+      const city2SunriseTime = results[1];
+
+      resultDiv.textContent = `City 1 Sunrise: ${city1SunriseTime}
+            City 2 Sunrise: ${city2SunriseTime}`;
+    })
+    .catch(function (error) {
+      resultDiv.textContent = "An error occurred while fetching sunrise times.";
+      console.error(error);
+    });
+});
+
+function fetchSunriseTime(latitude, longitude) {
+  const url = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}`;
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data.results.sunrise);
+}
+
