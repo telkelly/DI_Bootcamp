fe068db6cf5c34ef794d0850507d28021f069f1e
Kelly <84016657+telkelly@users.noreply.github.com>
2023-06-01 16:42:06 +0300

    created function fetchCurruncies that fetch api and putted it into html

--- week9/day5/currencyConverter/script.js
+++ week9/day5/currencyConverter/script.js
@@ -1,2 +1,35 @@
-const url =
-    "https://v6.exchangerate-api.com/v6/08235d2c70859153c9f806ad/latest/USD";
+fetchCurrencies();
+
+document.getElementById('convert').addEventListener("click", handleClick)
+
+function fetchCurrencies() {
+  const url =
+    "https://v6.exchangerate-api.com/v6/08235d2c70859153c9f806ad/codes";
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => populateDropdown(data.supported_codes))
+    .catch((err) => console.log(err));
+}
+
+function populateDropdown(codes) {
+  let entries = Object.entries(codes);
+  console.log(entries);
+  for (const entry of entries) {
+    const [index, [code,name]] = entry;
+    const option = document.createElement("option");
+    const option2 = document.createElement('option');
+
+    option.innerText = name;
+    option2.innerText = name;
+
+    option.value = index;
+    option2.value = index;
+    document.getElementById("from").appendChild(option);
+    document.getElementById("to").appendChild(option2);
+  }
+}
+
+function handleClick() {
+  
+}
