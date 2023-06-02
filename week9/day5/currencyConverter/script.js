14d156b0b9659258691c964401370121b3f0357b
Kelly <84016657+telkelly@users.noreply.github.com>
2023-06-02 22:56:18 +0300

    created function that convert currency

--- week9/day5/currencyConverter/script.js
+++ week9/day5/currencyConverter/script.js
@@ -1,6 +1,8 @@
 fetchCurrencies();
 
-document.getElementById('convert').addEventListener("click", handleClick)
+document.getElementById("convert").addEventListener("click", handleClick);
+const fromSelect = document.getElementById("from");
+const toSelect = document.getElementById("to");
 
 function fetchCurrencies() {
   const url =
@@ -16,20 +18,39 @@ function populateDropdown(codes) {
   let entries = Object.entries(codes);
   console.log(entries);
   for (const entry of entries) {
-    const [index, [code,name]] = entry;
+    const [index, [code, name]] = entry;
     const option = document.createElement("option");
-    const option2 = document.createElement('option');
+    const option2 = document.createElement("option");
 
-    option.innerText = name;
-    option2.innerText = name;
+    option.innerText = code + " ," + name;
+    option2.innerText = code + " ," + name;
 
-    option.value = index;
-    option2.value = index;
-    document.getElementById("from").appendChild(option);
-    document.getElementById("to").appendChild(option2);
+    option.value = code;
+    option2.value = code;
+    fromSelect.appendChild(option);
+    toSelect.appendChild(option2);
   }
 }
 
-function handleClick() {
+async function handleClick() {
+  const input = document.getElementById("input");
+
+  const curr1 = fromSelect.value;
+  const curr2 = toSelect.value;
+
+  const summary = document.getElementById('summary')
+
   
+
+  const url = `https://v6.exchangerate-api.com/v6/08235d2c70859153c9f806ad/pair/${curr1}/${curr2}`;
+  try {
+    const res = await fetch(url);
+    const resJson = await res.json();
+    const rate = resJson.conversion_rate;
+    const amount = Number(input.value);
+    const total = rate * amount;
+    console.log(total);
+  } catch (err) {
+    console.log(err);
+  }
 }
