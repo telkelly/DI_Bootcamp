687d36b491bb7d01bd503dd23ada0d3b888147b0
Kelly <84016657+telkelly@users.noreply.github.com>
2023-06-03 21:33:56 +0300

    solved 1st exercise

---
+++ week10/day4/exercisesXP/exercise-1.sql
@@ -0,0 +1,7 @@
+SELECT * FROM items ORDER BY item_price ASC
+
+SELECT * FROM items WHERE item_price >= 80 ORDER BY item_price 
+
+SELECT customer_name, customer_lastname FROM customers ORDER BY customer_name ASC
+
+SELECT customer_lastname FROM customers ORDER BY customer_lastname DESC;
