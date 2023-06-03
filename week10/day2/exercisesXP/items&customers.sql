feee2ce3f6aab51b41ab96c8b5fb9ace835c9ca5
Kelly <84016657+telkelly@users.noreply.github.com>
2023-06-03 15:03:19 +0300

    practice creating databases using postgreSQL and fetching data from the tables

---
+++ week10/day2/exercisesXP/items&customers.sql
@@ -0,0 +1,14 @@
+SELECT * FROM customers WHERE customer_lastname != 'Scott' 
+
+INSERT INTO items(item_name, item_price)
+	VALUES('Small Desk', 100),
+	('Large Desk', 300),
+	('Fan', 80);
+	
+	
+INSERT INTO customers(customer_name, customer_lastname)
+	VALUES('Greg', 'Jones'),
+	('Sandra', 'Jones'),
+	('Scott', 'Scott'),
+	('Trevor', 'Green'),
+	('Melanie', 'Johnson')
