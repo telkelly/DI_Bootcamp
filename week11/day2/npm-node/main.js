365efc11d40cd2a176d7ad420421f7a20db4b7eb
Kelly <84016657+telkelly@users.noreply.github.com>
2023-06-06 16:44:35 +0300

    initilized node

---
+++ week11/day2/npm-node/main.js
@@ -0,0 +1,14 @@
+// const a = 5;
+// const b = 10;
+
+// console.log(`Hello you are ${a+b}`);
+
+// console.log('Hi! How are you?');
+
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+
+console.log(x);
