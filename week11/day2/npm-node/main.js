7cbf6065e45549a0e25ba0dd8f7ade8ae5767803
Kelly <84016657+telkelly@users.noreply.github.com>
2023-06-08 21:06:08 +0300

    trainig node exrcises

--- week11/day2/npm-node/main.js
+++ week11/day2/npm-node/main.js
@@ -1,7 +1,11 @@
+// exrcise-1
+
 // const a = 5;
 // const b = 10;
 
 // console.log(`Hello you are ${a+b}`);
+
+// exercise-2
 
 // console.log('Hi! How are you?');
 
@@ -12,3 +16,4 @@ const c = 6;
 x = a + b;
 
 console.log(x);
+console.log(x + c);
