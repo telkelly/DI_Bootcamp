1f1ee45ed057e14fd4117a1f03595aebc842e53e
Kelly <84016657+telkelly@users.noreply.github.com>
2023-06-10 11:17:26 +0300

    put html tags in server

--- week11/day4/exerciseXP/node-server/myserver.js
+++ week11/day4/exerciseXP/node-server/myserver.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello port')
+     const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Server</title>
+    </head>
+    <body>
+      <h1>This is my first response</h1>
+      <h2>This is my second response</h2>
+      <p>This is my third response</p>
+    </body>
+    </html>
+  `;
+    res.end(html)
 })
 
 server.listen(3000, "localhost", () => {
