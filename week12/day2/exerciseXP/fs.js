5718deee3f58f11f13933660c1d81ef020df0ba0
Kelly <84016657+telkelly@users.noreply.github.com>
2023-06-15 17:10:55 +0300

    writed file and updated then deleted using file system

--- week12/day2/exerciseXP/fs.js
+++ week12/day2/exerciseXP/fs.js
@@ -7,3 +7,29 @@ fs.readFile('exercise1.txt', 'utf-8', (err,data) => {
         console.log(data);
     }
 })
+
+fs.writeFile('exercise2.txt', 'Xorosho snasibo kak tvoi?', (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('message sent');
+    }
+})
+
+const newContent = "Hey guys what are talking here about?";
+
+fs.appendFile('exercise2.txt', newContent, (e) =>{
+    if (e) {
+        console.log(e);
+    } else {
+        console.log('message updated');
+    }
+})
+
+fs.unlink('exercise2.txt', (err) => {
+     if (err) {
+       console.log(e);
+     } else {
+       console.log("message deleted");
+     }
+})
