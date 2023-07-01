60854230682d10ee328f1745ba8e10d7dc2ebe74
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-01 11:46:41 +0300

    created shuffledFuncition to shuffle between card

--- week14/day5/memory-game/src/App.js
+++ week14/day5/memory-game/src/App.js
@@ -10,27 +10,82 @@ const App = () => {
 
   useEffect(() => {
     setSuperheroes(cards.superheroes)
-  })
+  }, [])
 
-  const handleCardClick = () => {
-    
-  }
-  
+   
+
+  const handleCardClick = (id) => {
+    const clickedSuperhero = superheroes.find(superhero => superhero.id === id);
+
+    if (clickedSuperhero.clicked) {
+      setScore(0);
+      shuffleCards();
+      resetClickedStatus();
+    } else {
+      setScore(score + 1);
+      shuffleCards();
+      updateClickedStatus(id);
+    }
+  };
+
+  const shuffleCards = () => {
+    const shuffledSuperheroes = [...superheroes];
+    for (let i = shuffledSuperheroes.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledSuperheroes[i], shuffledSuperheroes[j]] = [
+        shuffledSuperheroes[j],
+        shuffledSuperheroes[i],
+      ];
+    }
+    setSuperheroes(shuffledSuperheroes);
+  };
+
+  const resetClickedStatus = () => {
+    setSuperheroes((prevSuperheroes) =>
+      prevSuperheroes.map((superhero) => ({
+        ...superhero,
+        clicked: false,
+      }))
+    );
+  };
+
+  const updateClickedStatus = (id) => {
+    setSuperheroes((prevSuperheroes) =>
+      prevSuperheroes.map((superhero) => {
+        if (superhero.id === id) {
+          return {
+            ...superhero,
+            clicked: true,
+          };
+        }
+        return superhero;
+      })
+    );
+  };
+
+ 
+
+  useEffect(() => {
+    if (score > topScore) {
+      setTopScore(score);
+    }
+  }, [score, topScore]);
+
+
   return (
     <div className="cards">
       <nav>
         <h2>Current Score: {score}</h2>
         <h2>Top Score: {topScore}</h2>
       </nav>
-      {superheroes.map(superhero => (
-          <Card
-            key={superhero.id}
-            superhero={superhero}
-            onClick={handleCardClick}
-          />
-        ))}
+      {superheroes.map((superhero) => (
+        <Card
+          key={superhero.id}
+          superhero={superhero}
+          onClick={handleCardClick}
+        />
+      ))}
     </div>
   );
 }
-
 export default App;
--- week14/day5/memory-game/src/components/Card.js
+++ week14/day5/memory-game/src/components/Card.js
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
 
 const Card = ({ superhero, onClick }) => {
-  const { id, name, image } = superhero;
+  const { id, name, occupation, image } = superhero;
 
   return (
     <div className="card" onClick={() => onClick(id)}>
       <img src={image} alt={name} />
       <h3>{name}</h3>
+      <p>{occupation}</p>
     </div>
   );
 };
