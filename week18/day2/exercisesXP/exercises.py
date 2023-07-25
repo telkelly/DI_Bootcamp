ca19278a676ed1a3908e2c0ab0f30d7f064c5440
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-25 07:32:55 +0300

    completed exercisesXP using python

---
+++ week18/day2/exercisesXP/exercises.py
@@ -0,0 +1,57 @@
+# exercise-1
+
+print("Hello world "*5)
+
+# exercise-2
+
+calculate = (99**3)*8
+print(calculate)
+
+# exercise-3
+
+# 5 < 3 -- false
+# 3 ==3 -- true
+# 3 == "3" -- false
+# "3" > 3 -- false
+# "Hello" == "hello" -- false
+
+# exercise-4
+
+computer_brand = "Mac"
+print(f"I have a {computer_brand} computer")
+
+# exercise-5
+
+name = "Kelly"
+age=23
+shoe_size = 39
+info=f"My name is Kelly and at the age {age} my foot size is {shoe_size}... that's strange"
+print(info)
+
+# exercise-6
+a=355
+b=95
+if(a>b):
+    print("Hello world")
+
+# exercise-7
+
+num = int(input("write number"))
+if(num%2==0):
+    print("even")
+else:
+    print("odd")
+
+# exercise-8
+user_name=input("write your name")
+if(name == user_name):
+    print("no way we are siblings")
+else:
+    print("your name is boring")
+
+# exercise-9
+height= int(input("write your heigth"))
+if(height==145):
+    print("you're tall enough to ride")
+else:
+    print("go home and grow bro")
