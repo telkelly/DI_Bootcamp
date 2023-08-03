3666e1b4131583c7ecb4ec5484ab652e008f9fe3
Kelly <84016657+telkelly@users.noreply.github.com>
2023-08-03 19:15:10 +0300

    created 3 functions which check if there is a winner in horizontal row or vertical row or giagonal and also function that checks tie and switch player function

--- week19/day5/tic-tac-toe/tic-tac-toe.py
+++ week19/day5/tic-tac-toe/tic-tac-toe.py
@@ -22,7 +22,69 @@ def playerInput(board):
     else:
         print("Oops player is already on this spot")
 
+# check for win or tie
+
+def checkHorizontle(board):
+    global winner
+    if board[0] == board[1] == board[2] != "_":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] != "_":
+        winner = board[3]
+        return True
+    elif board[5] == board[6] == board[7] != "_":
+        winner = board[5]
+        return True
+
+
+def checkVerticale(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "_":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "_":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "_":
+        winner = board[2]
+        return True
+
+def checkDiagonal(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "_":
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != "_":
+        winner = board[1]
+        return True
+
+def checkTie(board):
+    global gameRunning
+    if "_" not in board:
+        printBoard(board)
+        print("It's a tie")
+        gameRunning = False
+
+def switchPlayer(board):
+    global current_player
+    if current_player == "X":
+        print("Player O's turn")
+        current_player = "O"
+    else:
+        print("Player X's turn")
+        current_player = "X"
+
+def checkForWin():
+    global gameRunning
+    if checkDiagonal(board) or checkHorizontle(board) or checkVerticale(board):
+        print(f"The winner in {winner}")
+        gameRunning = False
+
+
 # start the game
 while gameRunning:
     printBoard(board)
     playerInput(board)
+    checkForWin()
+    checkTie(board)
+    switchPlayer(board)
