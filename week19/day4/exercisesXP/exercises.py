e2ad297adf5c040e7f056a15255c1f1e25b22578
Kelly <84016657+telkelly@users.noreply.github.com>
2023-08-05 19:30:21 +0300

    quiz on python

--- week19/day4/exercisesXP/exercises.py
+++ week19/day4/exercisesXP/exercises.py
@@ -26,6 +26,71 @@ def main():
         print("It's boiling! Don't forget to drink more water")
     print(f"The temperature right now is {random_temp} degrees Celsius.")
 
+#main()
+
+# exercise-2
+
+data = [
+    {
+        "question": "What is Baby Yoda's real name?",
+        "answer": "Grogu"
+    },
+    {
+        "question": "Where did Obi-Wan take Luke after his birth?",
+        "answer": "Tatooine"
+    },
+    {
+        "question": "What year did the first Star Wars movie come out?",
+        "answer": "1977"
+    },
+    {
+        "question": "Who built C-3PO?",
+        "answer": "Anakin Skywalker"
+    },
+    {
+        "question": "Anakin Skywalker grew up to be who?",
+        "answer": "Darth Vader"
+    },
+    {
+        "question": "What species is Chewbacca?",
+        "answer": "Wookiee"
+    }
+]
 
 
-main()
+def ask_questions():
+    correct_answers = 0
+    incorrect_answers = 0
+    wrong_answers = []
+
+    random.shuffle(data)
+
+    for question_data in data:
+        question = question_data["question"]
+        answer = question_data["answer"]
+
+        user_answer = input(question + " Write your answer: ")
+
+        if user_answer.lower() == answer.lower():
+            correct_answers += 1
+        else:
+            incorrect_answers += 1
+            wrong_answers.append((question, user_answer, answer))
+
+    print("Quiz completed.")
+    print(f"Number of correct answers: {correct_answers}")
+    print(f"Number of incorrect answers: {incorrect_answers}")
+
+    if incorrect_answers > 3:
+        print("You had more than 3 wrong answers. Please play again.")
+        print("Wrong answers and correct answers:")
+
+        for question, user_answer, correct_answer in wrong_answers:
+            print(f"Question: {question}")
+            print(f"Your Answer: {user_answer}")
+            print(f"Correct Answer: {correct_answer}")
+            print()
+
+ask_questions()
+
+# exercise-3
