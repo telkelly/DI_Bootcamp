18ee6df376c479875bf9b31556be3b3546830a2b
Kelly <84016657+telkelly@users.noreply.github.com>
2023-08-25 11:20:54 +0300

    ::

--- week20/day5/rps-game/game.py
+++ week20/day5/rps-game/game.py
@@ -1,17 +1,23 @@
 import random
 class Game:
     def __init__(self):
-        self.users_choice = input('Choice an item: (r)ock, (p)aper or (s)cissors ): ')
+        self.choice_list = ['r', 'p', 's']
+        self.users_choice = ''
         self.computers_choice = ''
+        self.scores = {'win': 0,
+                       'loss': 0,
+                       'draw': 0}
 
     def get_user_item(self):
-        while not self.users_choice:
-            self.users_choice
-        return self.users_choice
+        while True:
+            self.users_choice = input('Choice an item: (r)ock, (p)aper or (s)cissors ): ')
+            if self.users_choice in self.choice_list:
+                return self.users_choice
+            else:
+                return print("No answer. Choice an item: (r)ock, (p)aper or (s)cissors.")
 
     def get_computer_item(self):
-        random_list = ['r', 'p', 's']
-        return random.choice(random_list)
+        return random.choice(self.choice_list)
 
     def get_game_result(self):
         if self.users_choice == 'r' and self.computers_choice == 's' or self.users_choice == 'p' and self.computers_choice == 'r' or self.users_choice == 's' and self.computers_choice == 'p':
