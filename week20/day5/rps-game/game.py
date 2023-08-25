01ba685ae7c6a990c49d96fc1a668201a11be8a2
Kelly <84016657+telkelly@users.noreply.github.com>
2023-08-25 11:57:33 +0300

    fixed functions

--- week20/day5/rps-game/game.py
+++ week20/day5/rps-game/game.py
@@ -5,8 +5,8 @@ class Game:
         self.users_choice = ''
         self.computers_choice = ''
         self.scores = {'win': 0,
-                       'loss': 0,
-                       'draw': 0}
+                  'loss': 0,
+                  'draw': 0}
 
     def get_user_item(self):
         while True:
@@ -21,10 +21,14 @@ class Game:
 
     def get_game_result(self):
         if self.users_choice == 'r' and self.computers_choice == 's' or self.users_choice == 'p' and self.computers_choice == 'r' or self.users_choice == 's' and self.computers_choice == 'p':
+            self.scores['win'] += 1
             return print(f"You selected {self.users_choice}. The computer selected {self.computers_choice}. You win")
         elif self.users_choice == self.computers_choice:
+            self.scores['draw'] += 1
             return print(f"You selected {self.users_choice}. The computer selected {self.computers_choice}. You drew!")
-        return print(f"You selected {self.users_choice}. The computer selected {self.computers_choice}. You lose")
+        else:
+            self.scores['loss'] += 1
+            return print(f"You selected {self.users_choice}. The computer selected {self.computers_choice}. You lose")
 
     def play(self):
         self.get_user_item()
--- week20/day5/rps-game/rock-paper-scissors.py
+++ week20/day5/rps-game/rock-paper-scissors.py
@@ -1,14 +1,26 @@
 from game import Game
+
+
 class RockPaperScissors(Game):
     def __init__(self):
         self.user_choice = ''
 
     def get_user_menu_choice(self):
-        self.user_choice = input("Hey, it's Rock Paper Scissors game! Let's start? (p)lay a new game, (s)how scores, (q)uit")
+        self.user_choice = input(
+            "Hey, it's Rock Paper Scissors game! Let's start? (p)lay a new game, (s)how scores, (q)uit: ")
         return self.user_choice
-        # if user_choice == 'p':
-        #     Game.play()
-        # elif user_choice == 's'
-        #     self.print_results(res)
-        # return print("Bye Bye")
 
+    def print_results(self, results):
+        return print(f"Win: {results['win']}, Loss: {results['loss']}, Draw: {results['draw']}")
+
+    def main(self):
+        game = Game()
+        while True:
+            self.get_user_menu_choice()
+            if self.user_choice == 'p':
+                game.play()
+            elif self.user_choice == 's':
+                self.print_results(game.scores)
+
+new = RockPaperScissors()
+new.main()
