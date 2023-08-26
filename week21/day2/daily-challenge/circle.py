ff03a9db083cebc45818110b3bed8de4e205adc8
Kelly <84016657+telkelly@users.noreply.github.com>
2023-08-26 12:57:10 +0300

    started daily challenge

---
+++ week21/day2/daily-challenge/circle.py
@@ -0,0 +1,8 @@
+class Circle:
+    def __init__(self, radius=None, diameter=None):
+        if radius is not None:
+            self.radius = radius
+        elif diameter is not None:
+            self.radius = diameter / 2
+        else:
+            raise ValueError("Either radius or diameter must be provided")
