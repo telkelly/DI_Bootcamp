3f3f045fca21de3af143bd283bd7941265ba60f9
Kelly <84016657+telkelly@users.noreply.github.com>
2023-09-02 15:56:28 +0300

    can't get all animals in animal_in_family

--- week22/day2/exercisesXP/animal-info/animals/info/views.py
+++ week22/day2/exercisesXP/animal-info/animals/info/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from .data import animals
+from .data import animals, families
 
 
 # Create your views here.
-def my_view(request):
-    return render(request, 'animals/animal.html', {'data': animals})
+def display_all_animals(request):
+    return render(request, 'animals/all_animals.html', {'data': animals})
+
+
+def display_all_families(request):
+    return render(request, 'animals/all_families.html', {'data': families})
+
+
+def display_one_animal(request, animal_id):
+    animal = None
+    for entry in animals:
+        if entry.get('id') == animal_id:
+            animal = entry
+            break
+
+    return render(request, 'animals/animal.html', {'animal': animal})
+
+
+def display_animal_per_family(request, family_id):
+    match_animals = []
+    for entry in animals:
+        if entry.get('family') == family_id:
+            match_animals.append(entry)
+            break
+
+    return render(request, 'animals/animals_in_family.html', {'animal': match_animals})
