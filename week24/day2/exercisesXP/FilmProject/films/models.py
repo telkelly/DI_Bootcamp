19e6f761ef6941addba9a6f8329b1ae98846057a
Kelly <84016657+telkelly@users.noreply.github.com>
2023-09-11 17:24:37 +0300

    created models Film, Category, Country, Director

---
+++ week24/day2/exercisesXP/FilmProject/films/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Film(models.Model):
+    title = models.CharField(max_length=255)
+    release_date = models.DateField()
+    created_in_country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='films_created')
+    available_in_countries = models.ManyToManyField(Country, related_name='films_available')
+    category = models.ManyToManyField(Category)
+    director = models.ManyToManyField(Director)
