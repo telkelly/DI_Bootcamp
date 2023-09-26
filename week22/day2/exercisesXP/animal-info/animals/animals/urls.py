e18c97b93e5eaaea5c30fd54d45e8f0711fe7e69
Kelly <84016657+telkelly@users.noreply.github.com>
2023-09-26 06:28:27 +0300

    created new django project for prhone_number search and did model for Person

--- week22/day2/exercisesXP/animal-info/animals/animals/urls.py
+++ week22/day2/exercisesXP/animal-info/animals/animals/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import sys
+
+sys.path.append("..")
 from ..info import views
 
 urlpatterns = [
---
+++ week22/day4/daily-challenge/search_project/search/info/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+# Create your models here.
+class Phone(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 255)
+    email = models.EmailField(unique=True)
+    phone_number = PhoneNumberField()
+
+    def __str__(self):
+        return str(self.phone_number)
