675c3d5b5f111f8f6bdac9c5883e9a452eb2b5e7
Kelly <84016657+telkelly@users.noreply.github.com>
2023-09-30 12:47:51 +0300

    created user_profile view, buy_one_card, sell_one_card

--- week22/day5/exerciseXP/card_trading_website/cards/urls.py
+++ week22/day5/exerciseXP/card_trading_website/cards/urls.py
@@ -2,5 +2,9 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('all-cards/', views.display_all_cards, name='all-cards'),
+    path('cards/', views.display_all_cards, name='all-cards'),
+    path('cards/<int:card_id>/', views.display_one_card, name='card-detail'),
+    path('profile/<int:user_id>/', views.user_profile, name='user-profile'),
+    path('cards/<int:card_id>/buy/<user_id>', views.buy_one_card, name = 'buy-one-card'),
+    path('cards/<int:card_id>/sell/<user_id>', views.sell_one_card, name = 'sell-one-card'),
 ]
--- week22/day5/exerciseXP/card_trading_website/cards/views.py
+++ week22/day5/exerciseXP/card_trading_website/cards/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Card
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Card, User
 
 
 # Create your views here.
@@ -7,18 +7,65 @@ def display_all_cards(request):
     available_cards = Card.objects.all()
 
     context = {
-        'cards': available_cards
+        'cards':available_cards
     }
 
     return render(request, 'cards/cards.html', context)
 
 
 def display_one_card(request, card_id):
-    # Get the card object or return a 404 error if not found
-    card = get_object_or_404(Card, pk=card_id)
+    card = get_object_or_404(Card, pk = card_id)
 
     context = {
-        'card': card,
+        'card':card,
     }
 
-    return render(request, 'card_detail.html', context)
+    return render(request, 'cards/card_detail.html', context)
+
+
+def user_profile(request, user_id):
+    user = get_object_or_404(User, pk = user_id)
+
+    context = {
+        'user':user,
+    }
+
+    return render(request, 'users/user_profile.html', context)
+
+
+def buy_one_card(request, card_id, user_id):
+    card = get_object_or_404(Card, pk = card_id)
+    user = get_object_or_404(User, pk = user_id)
+
+    if user.amount_of_money >= card.price:
+        card.current_owner = user
+        card.save()
+
+        user.points += card.xp_points
+        user.save()
+
+        user.amount_of_money -= card.price
+        user.save()
+
+        return redirect('user-profile', user_id = user.id)
+    else:
+        return redirect('user-profile', user_id = user.id)
+
+
+def sell_one_card(request, card_id, user_id):
+    card = get_object_or_404(Card, pk = card_id)
+    user = get_object_or_404(User, pk = user_id)
+
+    if card.current_owner == user:
+        card.current_owner = None
+        card.save()
+
+        user.points -= card.xp_points
+        user.save()
+
+        card.previous_owner = user
+        card.save()
+
+        return redirect('home')
+
+    return redirect('user-profile', user_id = user.id)
