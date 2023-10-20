67b5aea499c271599b4de825e5403d67109eb599
Kelly <84016657+telkelly@users.noreply.github.com>
2023-10-20 19:06:33 +0300

    created managment folder to populate gifs but it still doesn't work

--- week23/day4/exerciseXP/gipny_project/gifs/managment/commands/populate_gifs.py
+++ week23/day4/exerciseXP/gipny_project/gifs/managment/commands/populate_gifs.py
@@ -1,14 +1,16 @@
 import random
 import requests
-from django.core.management import BaseCommand
+from django.core.management.base import BaseCommand
 from faker import Faker
-from models import Gif
+from gipny_project.gifs.models import Gif
 
 fake = Faker()
 categories = ['Funny', 'Cute', 'Sports', 'Movies', 'Animals', 'Dance', 'Memes', 'Music', 'Gaming', 'Science']
 
 
 class Command(BaseCommand):
+    help = 'Populate Gifs'
+
     def handle(self, *args, **kwargs):
         api_url = "https://api.giphy.com/v1/gifs/trending?limit=20&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"
         response = requests.get(api_url)
@@ -16,19 +18,18 @@ class Command(BaseCommand):
 
         Gif.objects.all().delete()
 
-        for gif in data:
+        for gif in data.get('data', []):
             gif_url = gif.get('url')
             gif_title = gif.get('title')
             uploader_name = fake.name()
             random_categories = random.sample(categories, 2)
 
-            gif = Gif(
-                gif_url = gif_url,
-                gif_title = gif_title,
-                uploader_name = uploader_name,
-                random_categories = random_categories
+            gif_instance = Gif(
+                gif_url=gif_url,
+                gif_title=gif_title,
+                uploader_name=uploader_name,
             )
 
-            gif.save()
+            gif_instance.save()
 
         self.stdout.write(self.style.SUCCESS('Successfully populated'))
