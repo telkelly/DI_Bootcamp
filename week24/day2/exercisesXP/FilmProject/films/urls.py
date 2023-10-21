d1076851423bfd313a1fe5eb1d0fa4e275dfc0a6
Kelly <84016657+telkelly@users.noreply.github.com>
2023-10-21 12:05:30 +0300

    added two links to the routes addFilm and addDirector

--- week24/day2/exercisesXP/FilmProject/films/urls.py
+++ week24/day2/exercisesXP/FilmProject/films/urls.py
@@ -3,6 +3,6 @@ from .views import HomePageView, DirectorCreateView, FilmCreateView
 
 urlpatterns = [
     path('homepage/', HomePageView.as_view(), name='homepage'),
-    path('add_director/', DirectorCreateView.as_view(), name='create_director'),
-    path('add_film/', FilmCreateView.as_view(), name='create_film'),
+    path('addFilm/', FilmCreateView.as_view(), name='addFilm'),
+    path('addDirector/', DirectorCreateView.as_view(), name='addDirector')
 ]
