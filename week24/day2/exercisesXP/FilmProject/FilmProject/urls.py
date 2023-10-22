9888cd6f4b9e8a8430df24209f8cd3c202ccece7
Kelly <84016657+telkelly@users.noreply.github.com>
2023-10-22 11:40:35 +0300

    created CustomUser based on AbstractBaseUser

--- week24/day2/exercisesXP/FilmProject/FilmProject/urls.py
+++ week24/day2/exercisesXP/FilmProject/FilmProject/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
+   # path('admin/', admin.site.urls),
    path('films/', include('films.urls')),
    path('film/', include('films.urls')),
    path('director/', include('films.urls')),
